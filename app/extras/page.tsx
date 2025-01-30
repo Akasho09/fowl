b1a1f185ae62f377452a8309bae1dbf330ae00bb
Akasho09 <aakashbwd@gmail.com>
2025-01-30 11:16:40 +0530

    extrass

--- app/extras/page.tsx
+++ app/extras/page.tsx
@@ -8,7 +8,7 @@ return <Layout><div className="h-screen md:w-[100vw] grid grid-cols-4">
 <ThreeTexts t1="Extras" t2="" t3=""></ThreeTexts>
 
 </div>
-<div className="col-span-1 border-r border-black flex justify-center items-center">
+<div className="col-span-4 md:col-span-1 border-r border-black flex justify-center items-center">
 <Image src="/image.png" width={300} height={300} alt="thank you"></Image>
 </div>
  <div className="col-span-2 h-full flex flex-col justify-center items-center hidden md:flex ">
