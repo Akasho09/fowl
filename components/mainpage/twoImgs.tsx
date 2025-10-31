82e9de590e7dc76f7fc511721ebde4fbb1f036da
Akasho09 <aakashbwd@gmail.com>
2025-11-01 03:17:13 +0530

    fix

--- components/mainpage/twoImgs.tsx
+++ components/mainpage/twoImgs.tsx
@@ -5,28 +5,30 @@ export default function TwoImages() {
   return (
     <div className="h-screen overflow-y-auto small-scrollbar w-[95vw] md:w-[20rem] max-w-full text-sm md:text-sm/6">
       <div className="h-screen flex flex-col items-center justify-start pt-8 p-2 relative z-10">
+        {/* Top Image */}
         <Image
           src="/comp1.png"
-          alt="Image 1"
+          alt="Top Image"
           width={300}
           height={300}
           className="border-b transition-all duration-500"
           style={{ filter: "var(--img-filter)" }}
         />
 
-        <div
-          className="relative flex justify-center w-full"
-          style={{ height: "calc(100vh - 600px)" }}
-        >
-          <div
-            className="h-3 w-3 rounded-full shadow-lg animate-bounce-ball"
-            style={{ backgroundColor: "var(--ball-bg)" }}
-          />
+        {/* Bounce Area */}
+        <div className="relative flex justify-center w-full" style={{ height: "calc(100vh - 600px)" }}>
+          <div className="relative h-full w-3 flex justify-center">
+            <div
+              className="absolute h-3 w-3 rounded-full shadow-lg animate-bounce-ball"
+              style={{ backgroundColor: "var(--ball-bg)" }}
+            />
+          </div>
         </div>
 
+        {/* Bottom Image */}
         <Image
           src="/emps.png"
-          alt="Image 2"
+          alt="Bottom Image"
           width={300}
           height={300}
           className="border-t transition-all duration-500"
@@ -36,3 +38,4 @@ export default function TwoImages() {
     </div>
   );
 }
+
